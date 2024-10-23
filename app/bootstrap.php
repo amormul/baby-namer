7ac492023f6b7675c8abeebd239cfe6bbf89f9cb
amormul <ghafics@gmail.com>
2024-10-23 11:35:53 +0300

    init

---
+++ app/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+include_once __DIR__ . '/core/controllers/FormController.php';
+include_once __DIR__ . '/core/storages/DataStorage.php';
+include_once __DIR__ . '/core/validators/NameValidator.php';
--- app/config.php
+++ app/config.php
@@ -3,3 +3,6 @@ const ERRORS_MSG = [
     'empty_name' => 'Поле імені не може бути порожнім',
     'invalid_name' => 'Ім\'я має містити лише літери',
 ];
+
+// Визначте шлях до JSON-файлу
+const JSON_FILE_PATH = __DIR__ . '/storage/json/data.json'; // Змініть шлях, якщо потрібно
